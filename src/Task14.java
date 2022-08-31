943663fda3c7f035d9693c278f2cd2860b5eb868
Nikeym <nikeym63@yandex.ru>
2022-08-31 17:27:37 +0300

    full homework2

---
+++ src/Task14.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task14 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String phone = input.nextLine().toLowerCase(Locale.ROOT);
+        int cost = input.nextInt();
+
+        System.out.println(phone);
+        System.out.println(cost);
+
+        if (cost >= 50000 && cost <= 120000 && (phone.contains("samsung") || phone.contains("iphone"))) {
+            System.out.println("Можно купить");
+        } else {
+            System.out.println("Не подходит");
+        }
+
+    }
+}
